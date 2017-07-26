4a912632d50d0c843eada10703d700ff49fe1fe8
Dat Nguyen <tdat.nguyen93@gmail.com>
2017-07-26 14:40:23 -0400

    Adding DenseNet CVPR 2017 Award Paper and Hierarchical Tree!!!

---
+++ softmaxtree/Tree.py
@@ -0,0 +1,35 @@
+"""
+Implementation of Hierarchical Soft-Max Tree
+
+Input:
+-----
+lisa.names
+"""
+import numpy as np
+
+
+class Node(object):
+    name = ' '
+    index = -1
+    parent = None
+    children = None
+    is_leaf  = False
+
+
+class SoftMaxTree(object):
+    def __init__(self, tree_file):
+        self.tree = None
+
+    def update_probability(self, softmax_vector):
+        """
+        Return a new soft-max vector by level of tree
+        :param softmax_vector:
+        :return:
+        """
+        raise NotImplemented
+
+    def get_hierachy_probability(self, key):
+        raise NotImplementeds
+
+if __name__ == "__main__":
+    tree = []
