a6270a99de129100bb0d12c8396179a387f43939
Dat Nguyen <tdat.nguyen93@gmail.com>
2017-09-18 13:48:54 -0700

    Updated custom dataset generator

--- create_custom_dataset.py
+++ create_custom_dataset.py
@@ -1,5 +1,5 @@
 """
-This script will create a custom dataset for training and evaluation. It will generate:
+This script creates a custom dataset for training and evaluation. It will generate:
 
    + a CSV training file
    + a CSV validation file (optional: if split is enabled)
@@ -29,9 +29,10 @@ Return
          | --  testing_data.csv   # if split is enabled
 """
 import os
-import sys
 import csv
+import numpy as np
 from PIL import Image
+from utils.box import box_iou, Box
 from sklearn.model_selection import train_test_split
 
 from argparse import ArgumentParser
@@ -52,6 +53,8 @@ parser.add_argument('-s', '--split',
 
 
 def main():
+  IMG_INPUT = 608
+  SHRINK_FACTOR =32
   arguments = parser.parse_args()
   path           = arguments.path
   output_dir     = arguments.output_dir
@@ -67,20 +70,36 @@ def main():
 
     lines = f.readlines()
     print("Calculating Anchors....")
+    id = 0
     for line in lines:
         img_path, x1, y1, x2, y2, label = line.rstrip().split(",")
-
         xc, yc, w, h = convert_edges_to_centroid(x1, y1, x2, y2)
+
         with Image.open(img_path) as img:
             img_width, img_height = img.size
 
-        # aspect_ratio = [IMG_INPUT / float(img_width), IMG_INPUT / float(img_height)]
-        # box = Box(0, 0, float(w) * aspect_ratio[0] / SHRINK_FACTOR, float(h) * aspect_ratio[1] / SHRINK_FACTOR)
+        aspect_ratio = [IMG_INPUT / float(img_width), IMG_INPUT / float(img_height)]
+        box = Box(0, 0, float(w) * aspect_ratio[0] / SHRINK_FACTOR, float(h) * aspect_ratio[1] / SHRINK_FACTOR)
         gt_boxes.append(box)
+
+        if label not in categories:
+          categories[label] = id
+          id += 1
+
+    anchors, avg_iou = k_mean_cluster(number_anchors, gt_boxes)
+    print("K = : {:2} | AVG_IOU:{:-4f} ".format(number_anchors, avg_iou))
+
     print("Done!")
 
   categories_file = os.path.join(output_dir, 'categories.txt')
   anchors_file    = os.path.join(output_dir, 'anchors.txt')
+  with open(categories_file, 'w') as f:
+    for item in categories.items():
+      f.write(item[0])
+
+  with open(anchors_file,'w') as f:
+    for anchor in anchors:
+      f.write(anchor)
 
   # ###################################
   # Generate Training/Validation data #
@@ -118,6 +137,84 @@ def save_dataset(data, save_path):
       item = item.split(',')
       writer.writerow(item)
 
+
+def k_mean_cluster(n_anchors, gt_boxes, loss_convergence=1e-5):
+  """
+  Cluster anchors.
+  """
+  # initial random centroids
+  centroid_indices = np.random.choice(len(gt_boxes), n_anchors)
+  centroids = []
+  for centroid_index in centroid_indices:
+    centroids.append(gt_boxes[centroid_index])
+
+  # iterate k-means
+  anchors, avg_iou, loss = run_k_mean(n_anchors, gt_boxes, centroids)
+
+  while True:
+    anchors, avg_iou, curr_loss = run_k_mean(n_anchors, gt_boxes, anchors)
+    if abs(loss - curr_loss) < loss_convergence:
+      break
+    loss = curr_loss
+
+  return anchors, avg_iou
+
+
+def run_k_mean(n_anchors, boxes, centroids):
+  """Perform K-mean clustering on training ground truth to generate anchors.
+  In the paper, authors argues that generating anchors through anchors would improve Recall of the network
+
+  NOTE: Euclidean distance produces larger errors for larger boxes. Therefore, YOLOv2 did not use Euclidean distance
+        to measure calculate loss. Instead, it uses the following formula:
+
+                  d(box, centroid) = 1−IOU(box, centroid)
+
+  :param n_anchors: K-value , number of desired anchors box
+  :param boxes:      list of bounding box in format [x1, y1, w, h]
+  :param centroids:
+  :return:
+      new_centroids: set of new anchors
+      groups:        wth?
+      loss:          compared to current bboxes
+  """
+  loss = 0
+  groups = []
+  new_centroids = []
+  for i in range(n_anchors):
+    groups.append([])
+    new_centroids.append(Box(0, 0, 0, 0))
+
+  for box in boxes:
+    min_distance = 1
+    group_index = 0
+
+    for i, centroid in enumerate(centroids):
+      distance = 1 - box_iou(box, centroid)  # Used in YOLO9000
+      if distance < min_distance:
+        min_distance = distance
+        group_index = i
+
+    groups[group_index].append(box)
+    loss += min_distance
+    new_centroids[group_index].w += box.w
+    new_centroids[group_index].h += box.h
+
+  for i in range(n_anchors):
+    if len(groups[i]) == 0:
+      continue
+    new_centroids[i].w /= len(groups[i])
+    new_centroids[i].h /= len(groups[i])
+
+  iou = 0
+  counter = 0
+  for i, anchor in enumerate(new_centroids):
+    for gt_box in groups[i]:
+      iou += box_iou(gt_box, anchor)
+      counter += 1
+
+  avg_iou = iou / counter
+  # print("Average IOU: {:4f}".format(avg_iou))
+  return new_centroids, avg_iou, loss
 
 if __name__ == '__main__':
   main()
