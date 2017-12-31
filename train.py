c02f5d597cee8251ac7beea38453a1ef1c6b5bba
Dat Nguyen <tdat.nguyen93@gmail.com>
2017-12-30 18:55:01 -0800

    Updated training pipeline

--- train.py
+++ train.py
@@ -27,16 +27,21 @@ python train.py \
 --learning_rate 0.001
 
 """
-import os
-import keras
+import cv2
+import numpy as np
 import config as cfg
-from argparse import ArgumentParser
+
+import keras
+import tensorflow as tf
+import keras.backend as K
 
 from yolov2.zoo import yolov2_darknet19
 from yolov2.core.loss import yolov2_loss
 
-from yolov2.utils.parser import parse_config
+from yolov2.utils.parser import parse_config, parse_inputs
+from yolov2.utils.callbacks import create_callbacks
 
+from argparse import ArgumentParser
 
 parser = ArgumentParser(description="Train YOLOv2")
 parser.add_argument('--csv_file',
@@ -53,8 +58,6 @@ parser.add_argument('--batch_size', type=int, default=1,
 
 parser.add_argument('--learning_rate', type=float, default=0.00001)
 
-parser.add_argument('--initial_epoch',
-                    help='Initial epoch (helpful for restart training)', type=int, default=0)
 
 parser.add_argument('--backup_dir', type=str, default='./backup/',
                     help='Path to to backup model directory')
@@ -70,15 +73,17 @@ def _main_():
     batch_size    = args.batch_size
     epochs        = args.epochs
     learning_rate = args.learning_rate  # this model has been pre-trained, LOWER learning rate is needed
-    initial_epoch = args.initial_epoch
-    backup_dir  = args.backup_dir
+    backup_dir    = args.backup_dir
 
     # Config Anchors and encoding dict
     anchors, label_dict = parse_config(cfg)
 
-    if not os.path.exists(backup_dir):
-        os.makedirs(backup_dir)
-        print("A backup directory has been created")
+    # ###############
+    # PREPARE DATA  #
+    # ###############
+    # @TODO: use TFRecords ?
+    # @TODO: Create K-fold cross validation training procedure
+    inputs, labels = parse_inputs(training_path, label_dict)
 
     # ###################
     # Define Keras Model
@@ -92,37 +97,83 @@ def _main_():
         model.load_weights(weight_file)
         print("Weight file has been loaded in to model")
 
-    # Set up callbacks
-    tf_board = keras.callbacks.TensorBoard(log_dir       = './logs',
-                                           histogram_freq= 1,
-                                           write_graph   = True,
-                                           write_images  = True)
-
-    backup = keras.callbacks.ModelCheckpoint(backup_dir +
-                                             "best_%s-{epoch:02d}-{val_loss:.2f}.weights" % 'darknet19',
-                                             monitor          = 'val_loss',
-                                             save_weights_only= True,
-                                             save_best_only   = True)
-
     # ###################
     # COMPILE AND TRAIN #
     # ###################
     model.compile(optimizer= keras.optimizers.Adam(lr=learning_rate),
                   loss     = yolov2_loss(anchors, cfg.N_CLASSES))
 
-    # ###############
-    # PREPARE DATA  #
-    # ###############
-    # @TODO: Create K-fold cross validation training procedure
+    from sklearn.model_selection import train_test_split
 
-    # data   = parse_inputs(training_path, label_dict)
-    #
-    # # Load X, Y
-    # X, Y = data.keys, data
+    for current_epoch in range(epochs):
 
-    # model.summary()
-    # model.save_weights('yolov2.weights')
-    # model.fit_generator()
+        # Create 10-fold split
+        x_train, x_val = train_test_split(inputs, test_size=0.2)
+        y_train = [labels[k] for k in x_train]
+        y_val   = [labels[k] for k in x_val]
+
+        history = model.fit_generator(generator       = data_generator(x_train, y_train),
+                                      steps_per_epoch = 1000,
+                                      validation_data = data_generator(x_val, y_val),
+                                      validation_steps= 100,
+                                      verbose=1,
+                                      workers=0)
+
+
+def data_generator(images, labels, shuffle=True, batch_size=128):
+    batch = input_func(images, labels, shuffle, batch_size)
+    while True:
+        yield K.batch_get_value(batch)
+
+
+def input_func(images, labels, shuffle=True, batch_size=128):
+
+    def map_func(image, label):
+        img = cv2.imread(image)
+        height, width, _ = img.shape
+
+        output_width  = cfg.IMG_INPUT_SIZE / cfg.SHRINK_FACTOR
+        output_height = cfg.IMG_INPUT_SIZE / cfg.SHRINK_FACTOR
+
+        x_train = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        y_train = np.zeros((output_height, output_width,  cfg.N_ANCHORS, 5 + cfg.N_CLASSES))
+
+        for obj in label:
+            xc, yc, w, h, label_idx = obj  # convert label to int
+            one_hot_encoding = np.eye(cfg.N_CLASSES)[label_idx]
+
+            # convert to relative value. A cell in grid map
+            gt_label = np.concatenate([[xc/float(width), yc/float(height),
+                                        w/float(width), h/float(height)],
+                                       [1.0],
+                                       one_hot_encoding])
+
+            # @TODO: this can be done in loss function or SparseTensor
+            center_x = xc * output_width
+            center_y = yc * output_height
+            r = int(np.floor(center_x))
+            c = int(np.floor(center_y))
+
+            # Construct Feature map ground truth
+            if r < output_width and c < output_height:
+                y_train[c, r, :, :] = cfg.N_ANCHORS * [gt_label]
+
+        return [tf.cast(x_train, tf.float32), tf.cast(y_train, tf.float32)]
+
+    dataset = tf.data.Dataset.from_tensor_slices((images, labels))
+    dataset = dataset.map(map_func)
+
+    if shuffle:
+        dataset = dataset.shuffle(buffer_size=10000)
+
+    dataset = dataset.repeat()
+    dataset = dataset.batch(batch_size)
+    iterator = dataset.make_one_shot_iterator()
+
+    # This is tensors
+    inputs, target = iterator.get_next()
+
+    return inputs, target
 
 
 if __name__ == "__main__":
