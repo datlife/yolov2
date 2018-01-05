cd314dae900a699fa135317ceeb75bdf7b8752f6
Dat Nguyen <tdat.nguyen93@gmail.com>
2018-01-04 22:39:43 -0800

    Updated training pipeline

--- train.py
+++ train.py
@@ -3,28 +3,23 @@ Training object detector for YOLOv2 on new dataset.
 
 Assumption:
 ----------
-   * Configuration file `cfg.py` has been setup properly, including:
+   * Configuration file `cfg.yaml has been setup properly, including:
          + feature extractor type
          + image size input
          + number of classes
          + path to anchors file, categories
 
    * A training and validation CSV files in correct format have been generated. (could be done through `create_dataset.py`).
-   * A feature extractor has been pre-trained (darknet19, densenet or mobilenet in this case).
 
 Return
 -------
-   A weight file - `yolov2.weights` for evaluation
+
 
 Example:
 
 python train.py \
---train-path    dataset/combined_lisa/training_data.csv \
---val-path      dataset/combined_lisa/testing_data.csv  \
---weights       weights/yolo-coco.weights \
---batch         8 \
---epochs        200 \
---learning_rate 0.001
+--train-path    dataset/pascal/training_data.csv \
+--config        dataset/pascal/config.yml  \
 
 """
 import yaml
@@ -44,22 +39,18 @@ from argparse import ArgumentParser
 parser = ArgumentParser(description="Train YOLOv2")
 parser.add_argument('--csv_file', type=str, default=None,
                     help="Path to CSV training data set")
-
-parser.add_argument('--weights', type=str, default=None,
-                    help="Path to pre-trained weight file")
+parser.add_argument('--config', type=str, default=None,
+                    help="Path to config file")
 
 
 def _main_():
     # ###############
     # PARSE CONFIG  #
     # ###############
-    args          = parser.parse_args()
+    args = parser.parse_args()
+    cfg  = yaml.load(open(args.config, 'r'))
 
-    with open('config.yml', 'r') as stream:
-        cfg = yaml.load(stream)
-
-    training_path = args.csv_file
-
+    training_data = args.csv_file
     weight_file   = cfg['model']['weight_file']
     num_classes   = cfg['model']['num_classes']
     image_size    = cfg['model']['image_size']
@@ -67,15 +58,17 @@ def _main_():
 
     # Config Anchors and encoding dict
     label_dict = parse_label_map(cfg['label_map'])
-    anchors = np.array(cfg['anchors']) * (cfg['model']['image_size'] / 608.)
+
+    # @TODO: remove 608.
+    anchors = np.array(cfg['anchors']) / (608. / shrink_factor)
 
     # ###############
     # PREPARE DATA  #
     # ###############
-    # @TODO: use TFRecords ?
     inv_map = {v: k for k, v in label_dict.iteritems()}
-    inputs, labels = parse_inputs(training_path, inv_map)
+    inputs, labels = parse_inputs(training_data, inv_map)
 
+    # using tf.data.Dataset to generate training samples
     tfdata = TFData(num_classes, anchors, shrink_factor)
 
     # ###################
@@ -88,25 +81,33 @@ def _main_():
                              anchors    = anchors,
                              num_classes= num_classes)
 
-    if weight_file:
-        model.load_weights(weight_file)
-        print("Weight file has been loaded in to model")
-
     # ###################
     # COMPILE AND TRAIN #
     # ###################
 
-    # From config file
+    # Hyper-parameters
     training_cfg  = cfg['training_params']
     epochs        = training_cfg['epochs']
     batch_size    = training_cfg['batch_size']
     learning_rate = training_cfg['learning_rate']  # this model has been pre-trained, LOWER learning rate is needed
     backup_dir    = training_cfg['backup_dir']
 
-    loss_obj = YOLOV2Loss(anchors, num_classes, warm_up_steps=100)
+    objective_function = YOLOV2Loss(anchors, num_classes)
 
     model.compile(optimizer= keras.optimizers.Adam(lr=learning_rate),
-                  loss     = loss_obj.compute_loss)
+                  loss     = objective_function.compute_loss)
+
+    if weight_file:
+        model.load_weights(weight_file)
+        print("Weight file has been loaded in to model")
+
+    # initialize the weights of the detection (last) layer to avoid nan when just starting training
+    layer      = model.layers[-2]
+    new_kernel = np.random.normal(size=layer.get_weights()[0].shape) / ((image_size / shrink_factor) ** 2)
+    new_bias   = np.random.normal(size=layer.get_weights()[1].shape) / ((image_size / shrink_factor) ** 2)
+    layer.set_weights([new_kernel, new_bias])
+
+    model.summary()
 
     for current_epoch in range(epochs):
         # Create 10-fold split
@@ -114,15 +115,12 @@ def _main_():
         y_train = [labels[k] for k in x_train]
         y_val   = [labels[k] for k in x_val]
 
-        print("Number of training samples: {} || {}".format(len(x_train), len(y_train)))
-        print("Number of validation samples: {} || {}".format(len(x_val), len(y_val)))
-
-        history = model.fit_generator(generator       = tfdata.generator(x_train, y_train, image_size, batch_size),
-                                      steps_per_epoch = 1000,
-                                      validation_data = tfdata.generator(x_val, y_val, image_size, batch_size),
-                                      validation_steps= 100,
-                                      verbose         = 1,
-                                      workers         = 0)
+        model.fit_generator(generator       = tfdata.generator(x_train, y_train, image_size, batch_size),
+                            steps_per_epoch = 1000,
+                            validation_data = tfdata.generator(x_val, y_val, image_size, batch_size),
+                            validation_steps= 100,
+                            verbose         = 1,
+                            workers         = 0)
 
         # @TODO: Evaluate y_val and save summaries to TensorBoard
 
--- yolov2/core/loss.py
+++ yolov2/core/loss.py
@@ -1,24 +1,21 @@
 """
 Loss (Objective) Function definition for You Only Look Once version 2
 """
-import numpy as np
 import tensorflow as tf
-import keras.backend as K
 
 
 class YOLOV2Loss(object):
 
-    def __init__(self, anchors, num_classes, warm_up_steps=100):
+    def __init__(self, anchors, num_classes, warm_up_steps=50):
         self.anchors = anchors
         self.num_classes = num_classes
         self.warm_up_steps = warm_up_steps
-        self.curr_epochs = 0
 
     def compute_loss(self, y_true, y_pred):
 
         output_dims = tf.shape(y_pred)
-        height = output_dims[1]
-        width = output_dims[2]
+        height      = output_dims[1]
+        width       = output_dims[2]
 
         pred_boxes   = y_pred[..., 0:4]
         pred_conf    = y_pred[..., 4]
@@ -28,72 +25,81 @@ class YOLOV2Loss(object):
         gt_conf    = y_true[..., 4]
         gt_classes = y_true[..., 5:]
 
-        obj_cells   = tf.expand_dims(y_true[..., 4], -1)
-        noobj_cells = tf.expand_dims(1.0 - y_true[..., 4], -1)
+        obj_cells      = tf.expand_dims(y_true[..., 4], -1)
+        noobj_cells    = tf.expand_dims(1.0 - y_true[..., 4], -1)
 
-        #  Create offset map
-        cx   = tf.reshape(tf.tile(tf.range(width), [height]), [-1, height, width, 1])
-        cy   = tf.tile(tf.expand_dims(tf.range(height), -1), [1, width])
-        cy   = tf.reshape(cy, [-1, height, width, 1])
-        c_xy = tf.to_float(tf.stack([cx, cy], -1))
+        num_objects    = tf.reduce_sum(tf.to_float(obj_cells > 0.0))
+        num_no_objects = tf.reduce_sum(tf.to_float(noobj_cells > 0.0))
 
         with tf.name_scope('RegressionLoss'):
-
             # Convert [y1, x1, y2, x2] to [y_c, x_c, h, w] coordinates
-            gt_wh          = gt_boxes[..., 2:4]   - gt_boxes[..., 0:2]
-            pred_wh        = pred_boxes[..., 2:4] - pred_boxes[..., 0:2]
-            gt_centroids   = gt_boxes[..., 0:2]   + (gt_wh / 2.0)
+            gt_wh = gt_boxes[..., 2:4] - gt_boxes[..., 0:2]
+            pred_wh = pred_boxes[..., 2:4] - pred_boxes[..., 0:2]
+
+            gt_centroids = gt_boxes[..., 0:2] + (gt_wh / 2.0)
             pred_centroids = pred_boxes[..., 0:2] + (pred_wh / 2.0)
 
-            grid_centroids = c_xy + 0.5
-            grid_wh = tf.cast(tf.reshape(self.anchors, [1, 1, 1, len(self.anchors), 2]), tf.float32)
+            #  Create offset grid
+            cx = tf.reshape(tf.tile(tf.range(width), [height]), [-1, height, width, 1])
+            cy = tf.tile(tf.expand_dims(tf.range(height), -1), [1, width])
+            cy = tf.reshape(cy, [-1, height, width, 1])
 
-            obj_coord    = obj_cells * (tf.square(pred_centroids - gt_centroids) +
-                                        tf.square(tf.sqrt(pred_wh) - tf.sqrt(gt_wh)))
+            grid_centroids = 0.5 + tf.to_float(tf.stack([cx, cy], -1))
+            grid_anchors   = tf.cast(tf.reshape(self.anchors, [1, 1, 1, len(self.anchors), 2]), tf.float32)
 
+            # For cells containing objects, we penalize the difference between ground truths and predictions
+            obj_coord = obj_cells * (tf.square(pred_centroids - gt_centroids) +
+                                     tf.square(tf.sqrt(pred_wh) - tf.sqrt(gt_wh)))
+
+            # For cells containing no objects, we penalize the difference between predictions
+            # and the centroids + anchors of that cells
             no_obj_coord = noobj_cells * (tf.square(pred_centroids - grid_centroids) +
-                                          tf.square(tf.sqrt(pred_wh) - tf.sqrt(tf.ones_like(pred_wh) * grid_wh)))
+                                          tf.square(tf.sqrt(pred_wh) - tf.sqrt(tf.ones_like(pred_wh) * grid_anchors)))
+            # Average out
+            obj_coord = tf.reduce_sum(obj_coord) / (num_objects + 1e-6)
+            no_obj_coord = tf.reduce_sum(no_obj_coord) / (num_no_objects + 1e-6)
 
             coord_loss = 5.0 * obj_coord + 0.01 * no_obj_coord
 
         with tf.name_scope('ObjectConfidenceLoss'):
-            box_iou = compute_iou(gt_boxes, pred_boxes)
+            box_iou = self.compute_iou(gt_boxes, pred_boxes)
             best_iou = tf.expand_dims(tf.reduce_max(box_iou, axis=-1), -1)
 
-            obj_conf    = obj_cells   * tf.expand_dims(tf.square(pred_conf - gt_conf * box_iou), -1)
+            obj_conf = obj_cells * tf.expand_dims(tf.square(pred_conf - gt_conf * box_iou), -1)
             no_obj_conf = noobj_cells * tf.expand_dims(tf.square(pred_conf - 0.0) * tf.to_float(best_iou < 0.6), -1)
+
+            obj_conf = tf.reduce_sum(obj_conf) / (num_objects + 1e-6)
+            no_obj_conf = tf.reduce_sum(no_obj_conf) / (num_no_objects + 1e-6)
+
             conf_loss = 1.0 * obj_conf + 0.01 * no_obj_conf
 
-        with tf.name_scope("ClassificationLoss"):
-            cls_loss = tf.nn.softmax_cross_entropy_with_logits(labels=gt_classes, logits=pred_classes)
-            cls_loss = 1.0 * obj_cells * tf.expand_dims(cls_loss, -1)
-
-        coord_loss = tf.Print(coord_loss, [tf.reduce_sum(coord_loss)], "[INFO] Coordinate Loss:")
-        conf_loss = tf.Print(conf_loss, [tf.reduce_sum(conf_loss)], "[INFO] Confidence Loss:")
-        cls_loss = tf.Print(cls_loss, [tf.reduce_sum(cls_loss)], "[INFO] Classfication Loss:")
-
-        return tf.reduce_sum(coord_loss) + tf.reduce_sum(conf_loss) + tf.reduce_sum(cls_loss)
         # # @TODO: focal loss
+        with tf.name_scope("ClassificationLoss"):
 
+            cls_loss = tf.nn.softmax_cross_entropy_with_logits(labels=gt_classes, logits=pred_classes)
+            cls_loss = obj_cells * tf.expand_dims(cls_loss, -1)
+            cls_loss = 1.0 * tf.reduce_sum(cls_loss) / (num_objects + 1e-6)
 
-def compute_iou(gt_boxes, pred_boxes, scope=None):
-    with tf.name_scope(scope, 'IoU'):
-        with tf.name_scope('area'):
-            gt_wh = gt_boxes[..., 2:4] - gt_boxes[..., 0:2]
-            pr_wh = pred_boxes[..., 2:4] - pred_boxes[..., 0:2]
+        return coord_loss + conf_loss + cls_loss
 
-            gt_area = gt_wh[..., 0] * gt_wh[..., 1]
-            pr_area = pr_wh[..., 0] * pr_wh[..., 1]
+    def compute_iou(self, gt_boxes, pred_boxes, scope=None):
+        with tf.name_scope(scope, 'IoU'):
+            with tf.name_scope('area'):
+                gt_wh = gt_boxes[..., 2:4] - gt_boxes[..., 0:2]
+                pr_wh = pred_boxes[..., 2:4] - pred_boxes[..., 0:2]
 
-        with tf.name_scope('intersections'):
-            intersect_mins = tf.maximum(gt_boxes[..., 0:2], pred_boxes[..., 0:2])
-            intersect_maxes = tf.minimum(gt_boxes[..., 2:4], pred_boxes[..., 2:4])
-            intersect_hw = tf.maximum(intersect_maxes - intersect_mins, 0.)
-            intersections = intersect_hw[..., 0] * intersect_hw[..., 1]
+                gt_area = gt_wh[..., 0] * gt_wh[..., 1]
+                pr_area = pr_wh[..., 0] * pr_wh[..., 1]
 
-        unions = gt_area + pr_area - intersections
-        iou = tf.where(tf.equal(intersections, 0.0),
-                       tf.zeros_like(intersections),
-                       tf.truediv(intersections, unions))
+            with tf.name_scope('intersections'):
+                intersect_mins = tf.maximum(gt_boxes[..., 0:2], pred_boxes[..., 0:2])
+                intersect_maxes = tf.minimum(gt_boxes[..., 2:4], pred_boxes[..., 2:4])
+                intersect_hw = tf.maximum(intersect_maxes - intersect_mins, 0.)
+                intersections = intersect_hw[..., 0] * intersect_hw[..., 1]
 
-        return iou
+            unions = gt_area + pr_area - intersections
+            iou = tf.where(tf.equal(intersections, 0.0),
+                           tf.zeros_like(intersections),
+                           tf.truediv(intersections, unions))
+
+            return iou
