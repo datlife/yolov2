a3b5153b0110e616648d6983120fe210a58bcc89
Dat Nguyen <tdat.nguyen93@gmail.com>
2018-01-15 01:36:41 -0800

    working k-mean

--- dataset/generate_config_file.py
+++ dataset/generate_config_file.py
@@ -33,11 +33,10 @@ Return
          | --  anchors.txt
          | --  training_data.csv
 """
-# https://www.tensorflow.org/programmers_guide/datasets
-
 import os
-import logging
 import csv
+import time
+import random
 import numpy as np
 from PIL import Image
 from argparse import ArgumentParser
@@ -63,9 +62,11 @@ def main():
   # #################################
   # Generate Anchors and Categories #
   # #################################
+  start_reading_time = time.time()
   with open(data_file, 'r') as fio:
-    logging.info("Reading data")
+    print("[INFO] Reading data")
     gt_boxes = []
+    next(fio)  # skip header line @TODO: handle this in better way
     for line in fio:
       img_path, x1, y1, x2, y2, label = line.rstrip().split(",")
       xc, yc, w, h = convert_edges_to_centroid(x1, y1, x2, y2)
@@ -73,18 +74,135 @@ def main():
         img_width, img_height = img.size
 
       # Convert to relative bboxes
-      relative_bbox = np.array([xc, yc, w, h])/np.array([img_width, img_height, img_width, img_height], dtype=np.float)
+      relative_bbox = np.array([0, 0, w, h]) / np.array([img_width, img_height,
+                                                         img_width, img_height],
+                                                        dtype=np.float)
       gt_boxes.append(relative_bbox)
 
-    logging.info("Calculating Anchors using K-mean Clustering....")
-    anchors, avg_iou = k_mean_cluster(num_anchors, gt_boxes)
-    logging.info("Number of anchors: {:2} | Average IoU:{:-4f}\n\n ".format(num_anchors, avg_iou))
-
-  print(anchors)
-
+    completed_reading_time = time.time()
+    print("[INFO] File is loaded in {:3} secs".format(completed_reading_time - start_reading_time))
+    print("[INFO] Running K mean clustering")
+    anchors, avg_iou = k_mean_cluster(num_anchors, np.array(gt_boxes))
+    print("[INFO] Completed in {:2f} secs".format(time.time() - completed_reading_time))
+    # print("Number of anchors: {:2} | Average IoU:{:-4f}\n ".format(num_anchors, avg_iou))
+    print(avg_iou)
+    print(anchors[..., 2:4])
   # ###################################
   # Generate Training/Validation data #
   # ###################################
+
+
+def k_mean_cluster(num_anchors, gt_boxes, loss_convergence=1e-7):
+  """Cluster anchors.
+  """
+  # Randomly pick 5 boxes as centroids
+  centroids = np.array(random.sample(gt_boxes, k=num_anchors))
+  anchors, loss = run_k_mean(num_anchors, gt_boxes, centroids)
+  while True:
+    anchors, curr_loss = run_k_mean(num_anchors, gt_boxes, anchors)
+    if abs(loss - curr_loss) < loss_convergence:
+      break
+    loss = curr_loss
+
+  ious    = compute_iou(gt_boxes, anchors)
+  avg_iou = np.mean(np.max(ious, axis=-1))
+  return anchors, avg_iou
+
+
+def run_k_mean(num_anchors, boxes, centroids):
+  """Perform K-mean clustering on training ground truth to generate anchors.
+
+  In the paper, authors argues that generating anchors through k-mean clustering would improve Recall of the network
+
+  NOTE:YOLOv2 uses the following formula to calculate distance:
+            d(box, centroid)= 1 - IoU (box, centroid)
+  Args:
+    num_anchors:
+    boxes:
+    centroids:
+
+  Returns:
+      new_centroids: set of new anchors
+      groups:        wth?
+      loss:          compared to current bboxes
+  """
+  # Initialize updated anchors with 0.0
+  updated_centroids = np.zeros_like(centroids)
+
+  # Measure distances between all bboxes and current centroids
+  distances = 1.0 - compute_iou(boxes, centroids)
+
+  # Find the closest centroids which a box should belong too
+  closest_centroids = np.argmin(distances, axis=-1)
+
+  # Now, update centroids by taking average of xc, yc, w, h
+  for box, idx in zip(boxes, closest_centroids):
+    updated_centroids[idx] += box
+
+  for i, freq in zip(*(np.unique(closest_centroids, return_counts=True))):
+    updated_centroids[i] /= freq
+
+  loss    = np.sum(np.min(distances, axis=-1))
+  return updated_centroids, loss
+
+
+def compute_iou(boxes1, boxes2):
+  """
+  Args:
+    boxes1: - a np.array of N boxes [N, 4]
+    boxes2: - a np.array of M boxes [M, 4]
+
+  Returns:
+    a IoU matrix [N, M]
+
+  """
+  areas1  = area(boxes1)
+  areas2  = area(boxes2)
+  intersections = intersection(boxes1, boxes2)
+
+  unions = (np.expand_dims(areas1, 1) +
+            np.expand_dims(areas2, 0) - intersections)
+
+  return np.where(np.equal(intersections, 0.0),
+                  np.zeros_like(intersections),
+                  np.divide(intersections, unions))
+
+
+def area(boxes):
+  """Computes area of boxes.
+
+  Args:
+    boxes: a np.array holding N boxes [N, 4]
+
+  Returns:
+    a tensor with shape [N] representing box areas.
+  """
+  y_min, x_min, y_max, x_max = np.split(boxes, 4, axis=-1)
+  return np.squeeze((y_max - y_min) * (x_max - x_min), [1])
+
+
+def intersection(boxes1, boxes2):
+  """Compute pairwise intersection areas between boxes.
+  Args:
+    boxes1:   a np.array holding N boxes [N, 4]
+    boxes2:  a np.array holding M boxes [N, 4]
+
+  Returns:
+      a np.array shape [N, M] representing pairwise intersections
+  """
+  y_min1, x_min1, y_max1, x_max1 = np.split(boxes1, 4, axis=-1)
+  y_min2, x_min2, y_max2, x_max2 = np.split(boxes2, 4, axis=-1)
+
+  all_pairs_min_ymax = np.minimum(y_max1, np.transpose(y_max2))
+  all_pairs_max_ymin = np.maximum(y_min1, np.transpose(y_min2))
+
+  intersect_heights  = np.maximum(0.0, all_pairs_min_ymax - all_pairs_max_ymin)
+  all_pairs_min_xmax = np.minimum(x_max1, np.transpose(x_max2))
+  all_pairs_max_xmin = np.maximum(x_min1, np.transpose(x_min2))
+
+  intersect_widths = np.maximum(0.0, all_pairs_min_xmax - all_pairs_max_xmin)
+
+  return intersect_heights * intersect_widths
 
 
 def convert_edges_to_centroid(x1, y1, x2, y2):
@@ -104,83 +222,34 @@ def save_dataset(data, save_path):
       writer.writerow(item)
 
 
-def k_mean_cluster(n_anchors, gt_boxes, loss_convergence=1e-5):
-  """
-  Cluster anchors.
-  """
-  # initialize random centroids
-  centroid_indices = np.random.choice(len(gt_boxes), n_anchors)
-  centroids = []
-  for centroid_index in centroid_indices:
-    centroids.append(gt_boxes[centroid_index])
-
-  # iterate k-means
-  anchors, avg_iou, loss = run_k_mean(n_anchors, gt_boxes, centroids)
-
-  while True:
-    anchors, avg_iou, curr_loss = run_k_mean(n_anchors, gt_boxes, anchors)
-    if abs(loss - curr_loss) < loss_convergence:
-      break
-    loss = curr_loss
-
-  return anchors, avg_iou
-
-
-def run_k_mean(n_anchors, boxes, centroids):
-  '''
-  Perform K-mean clustering on training ground truth to generate anchors.
-  In the paper, authors argues that generating anchors through anchors would improve Recall of the network
-
-  NOTE: Euclidean distance produces larger errors for larger boxes. Therefore, YOLOv2 did not use Euclidean distance
-        to measure calculate loss. Instead, it uses the following formula:
-        d(box, centroid)= 1 - IoU (box, centroid)
-
-  :param n_anchors:
-  :param boxes:
-  :param centroids:
-  :return:
-      new_centroids: set of new anchors
-      groups:        wth?
-      loss:          compared to current bboxes
-  '''
-  loss = 0
-  groups = []
-  new_centroids = []
-  for i in range(n_anchors):
-    groups.append([])
-    new_centroids.append(Box(0, 0, 0, 0))
-
-  for box in boxes:
-    min_distance = 1
-    group_index = 0
-
-    for i, centroid in enumerate(centroids):
-      distance = 1 - box_iou(box, centroid)  # Used in YOLO9000
-      if distance < min_distance:
-        min_distance = distance
-        group_index = i
-
-    groups[group_index].append(box)
-    loss += min_distance
-    new_centroids[group_index].w += box.w
-    new_centroids[group_index].h += box.h
-
-  for i in range(n_anchors):
-    if len(groups[i]) == 0:
-      continue
-    new_centroids[i].w /= len(groups[i])
-    new_centroids[i].h /= len(groups[i])
-
-  iou = 0
-  counter = 0
-  for i, anchor in enumerate(new_centroids):
-    for gt_box in groups[i]:
-      iou += box_iou(gt_box, anchor)
-      counter += 1
-
-  avg_iou = iou / counter
-  return new_centroids, avg_iou, loss
-
-
 if __name__ == '__main__':
   main()
+
+ # for box in boxes:
+ #    min_distance = 1
+ #    group_index = 0
+ #
+ #    for i, centroid in enumerate(centroids):
+ #      # Used in YOLO9000
+ #      distance = 1 - box_iou(box, centroid)
+ #      if distance < min_distance:
+ #        min_distance = distance
+ #        group_index = i
+ #
+ #    groups[group_index].append(box)
+ #    loss += min_distance
+ #    new_centroids[group_index].w += box.w
+ #    new_centroids[group_index].h += box.h
+ #
+ #  for i in range(num_anchors):
+ #    if len(groups[i]) == 0:
+ #      continue
+ #    new_centroids[i].w /= len(groups[i])
+ #    new_centroids[i].h /= len(groups[i])
+ #
+ #  iou = 0
+ #  counter = 0
+ #  for i, anchor in enumerate(new_centroids):
+ #    for gt_box in groups[i]:
+ #      iou += box_iou(gt_box, anchor)
+ #      counter += 1
